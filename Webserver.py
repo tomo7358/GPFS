94c2f9863d05a0e2a273aafc8af86d96895df149
Tomo7358 <thomas.wyzykoski@gmail.com>
2023-01-25 00:32:53 -0700

    Have error that I can't solve

--- Webserver.py
+++ Webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,g,redirect,url_for,session
 from wtforms import Form, FileField, validators
 from wtforms import SelectMultipleField
 
@@ -6,7 +6,6 @@ import os
 import random
 import pandas as pd
 import SMBH
-
 app = Flask(__name__,template_folder='/home/kronos/GPFS-1/Templates')
 # clear tmp directory
 dir = '/home/kronos/GPFS-1/tmp'
@@ -14,9 +13,9 @@ for f in os.listdir(dir):
     os.remove(os.path.join(dir, f))
 
 # configure the app
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = 'udOAg]!YzPC}=%WW}3"+K>E*[x7`XV&iGExmEDz>|4fbL$a51{J.VNQW7_9a4oO'
 app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024
-
+# identify unit form
 
 class FileForm(Form):
     file = FileField('File')
@@ -25,51 +24,47 @@ class TaskForm(Form):
     tasks = SelectMultipleField('Tasks')
 
 @app.route('/upload', methods=['GET', 'POST'])
-@app.route('/identify_unit', methods=['GET', 'POST'])
-def upload():
-    marks = SMBH.Markbook()
 
+
+def identify_unit():
+
+        marks = SMBH.Markbook()
+        marks.extract_tables(session['full_path'])
+        df=marks.data()
+        form = TaskForm()
+        form.tasks.choices = [(task, task) for task in df["Task"]]
+        if request.method == 'POST':
+            selected_tasks = request.form.getlist('tasks')
+            grade_categories = [task for task in selected_tasks if request.form.get(task) == 'grade']
+            unit_categories = [task for task in selected_tasks if request.form.get(task) == 'unit']
+            for index, row in df.iterrows():
+                task = row["Task"]
+                if task in grade_categories:
+                    current_unit = None
+                elif task in unit_categories:
+                    current_unit = task
+                    current_grade_group = None
+                    df.at[index, "Unit"] = current_unit
+                    df.at[index, "Grade Group"] = current_grade_group
+                    df["Grade Group"] = df["Grade Group"].fillna(method='ffill')
+                    return str(df)
+        return render_template('identify_unit.html', form=form)
+
+
+
+def upload():
     form = FileForm()
     if request.method == 'POST':
-        try:
-            file = request.files['file']
-            # Use the secure_filename() function to generate a safe filename
-            # and the os.path.join() function to join the directory and filename
-            filename = str(random.randint(1000000000,9999999999))
-            print(filename)
-            file.save(os.path.join("/home/kronos/GPFS-1/tmp", filename+'.pdf'))
-            full_path = os.path.join("/home/kronos/GPFS-1/tmp", filename+'.pdf')
-            marks.extract_tables(full_path)
-            print(marks.data())
-            # use your module to convert the pdf to a dataframe
-            return render_template('identify_unit.html', form=form, df=df)
-        except Exception as e:
-            return(str(e))
+        file = request.files['file']
+        filename = str(random.randint(1000000000,9999999999))
+        file.save(os.path.join("/home/kronos/GPFS-1/tmp", filename+'.pdf'))
+        session['full_path'] = os.path.join("/home/kronos/GPFS-1/tmp", filename+'.pdf')
+        
+        return redirect(url_for('identify_unit'))
     return render_template('Upload.html', form=form)
 
-def identify_unit(df):
-    form = TaskForm()
-    form.tasks.choices = [(task, task) for task in df["Task"]]
-    # render the form template and allow the user to select the tasks
-    # ...
-    if request.method == 'POST':
-        # get the selected tasks and their corresponding categories
-        selected_tasks = request.form.getlist('tasks')
-        grade_categories = [task for task in selected_tasks if request.form.get(task) == 'grade']
-        unit_categories = [task for task in selected_tasks if request.form.get(task) == 'unit']
-        # update the dataframe based on the user's selections
-        for index, row in df.iterrows():
-            task = row["Task"]
-            if task in grade_categories:
-                current_grade_group = task
-                current_unit = None
-            elif task in unit_categories:
-                current_unit = task
-                current_grade_group = None
-            df.at[index, "Unit"] = current_unit
-            df.at[index, "Grade Group"] = current_grade_group
-        df["Grade Group"] = df["Grade Group"].fillna(method='ffill')
-    return df
+
+
 
 
 
---
+++ compare.py
@@ -0,0 +1,100 @@
+from flask import Flask, render_template, request
+from wtforms import Form, FileField, validators
+from wtforms import SelectMultipleField
+
+import os
+import random
+import pandas as pd
+import SMBH
+
+app = Flask(__name__,template_folder='/home/kronos/GPFS-1/Templates')
+# clear tmp directory
+dir = '/home/kronos/GPFS-1/tmp'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
+
+# configure the app
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024
+# identify unit form
+
+class FileForm(Form):
+    file = FileField('File')
+
+class TaskForm(Form):
+    tasks = SelectMultipleField('Tasks')
+
+@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/identify_unit', methods=['GET', 'POST'])
+
+def identify_unit(df):
+    
+    form = TaskForm()
+    form.tasks.choices = [(task, task) for task in df["Task"]]
+    if request.method == 'POST':
+        selected_tasks = request.form.getlist('tasks')
+        grade_categories = [task for task in selected_tasks if request.form.get(task) == 'grade']
+        unit_categories = [task for task in selected_tasks if request.form.get(task) == 'unit']
+        for index, row in df.iterrows():
+            task = row["Task"]
+            if task in grade_categories:
+                current_grade_group = task
+                current_unit = None
+            elif task in unit_categories:
+                current_unit = task
+                current_grade_group = None
+                df.at[index, "Unit"] = current_unit
+                df.at[index, "Grade Group"] = current_grade_group
+                df["Grade Group"] = df["Grade Group"].fillna(method='ffill')
+                return render_template('result.html', df=df)
+    return render_template('identify_unit.html', form=form)
+
+def upload():
+    marks = SMBH.Markbook()
+
+    form = FileForm()
+    if request.method == 'POST':
+        try:
+            file = request.files['file']
+            # Use the secure_filename() function to generate a safe filename
+            # and the os.path.join() function to join the directory and filename
+            filename = str(random.randint(1000000000,9999999999))
+            print(filename)
+            file.save(os.path.join("/home/kronos/GPFS-1/tmp", filename+'.pdf'))
+            full_path = os.path.join("/home/kronos/GPFS-1/tmp", filename+'.pdf')
+            marks.extract_tables(full_path)
+            print(marks.data())
+            # use your module to convert the pdf to a dataframe
+            return render_template('identify_unit.html', form=form, df=marks.data())
+        except Exception as e:
+            return(str(e))
+    return render_template('Upload.html', form=form)
+
+def identifed_unit(df):
+    form = TaskForm()
+    form.tasks.choices = [(task, task) for task in df["Task"]]
+    # render the form template and allow the user to select the tasks
+    # ...
+    if request.method == 'POST':
+        # get the selected tasks and their corresponding categories
+        selected_tasks = request.form.getlist('tasks')
+        grade_categories = [task for task in selected_tasks if request.form.get(task) == 'grade']
+        unit_categories = [task for task in selected_tasks if request.form.get(task) == 'unit']
+        # update the dataframe based on the user's selections
+        for index, row in df.iterrows():
+            task = row["Task"]
+            if task in grade_categories:
+                current_grade_group = task
+                current_unit = None
+            elif task in unit_categories:
+                current_unit = task
+                current_grade_group = None
+            df.at[index, "Unit"] = current_unit
+            df.at[index, "Grade Group"] = current_grade_group
+        df["Grade Group"] = df["Grade Group"].fillna(method='ffill')
+        return df
+
+
+
+app.run(debug=True)
+              
